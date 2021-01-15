b868f9fb40cd05013f3481fd1ed371919603fbc6
ryuheimat <ryuheimat3@gmail.com>
2021-01-15 16:09:01 +0300

    update test cases for staking reward

--- test/TippingAndPool.js
+++ test/TippingAndPool.js
@@ -10,6 +10,7 @@ contract('Tipping', accounts => {
     const account_one = accounts[0];
     const account_two = accounts[1];
     const account_three = accounts[2];
+    const account_four = accounts[3];
 
     beforeEach(async function() {
        this.libertas = await LibertasToken.new();
@@ -58,6 +59,81 @@ contract('Tipping', accounts => {
             const address = this.stakingPool.address;
             address.should.not.equal(null);
         });
+
+        it('should be able to deposit into the pool', async function() {
+            await this.libertas.transfer(account_two, 1000, {from: account_one});
+            await this.libertas.approve(this.stakingPool.address, 300, {from: account_two});
+            await this.stakingPool.deposit(300, {from: account_two});
+
+            let balance_account_two = await this.libertas.balanceOf.call(account_two);
+            balance_account_two = balance_account_two.toNumber();
+            balance_account_two.should.equal(700);
+
+            let userInfo = await this.stakingPool.userInfo.call(account_two);
+            deposited_account_two = userInfo.amount;
+            deposited_account_two = deposited_account_two.toNumber();
+            deposited_account_two.should.equal(300);
+        });
+
+        it('should be able to withdraw from the pool', async function() {
+            await this.libertas.transfer(account_two, 1000, {from: account_one});
+            await this.libertas.approve(this.stakingPool.address, 300, {from: account_two});
+            await this.stakingPool.deposit(300, {from: account_two});
+            await this.stakingPool.withdraw(200, {from: account_two});
+            
+            let balance_account_two = await this.libertas.balanceOf.call(account_two);
+            balance_account_two = balance_account_two.toNumber();
+            balance_account_two.should.equal(900);
+
+            let userInfo = await this.stakingPool.userInfo.call(account_two);
+            deposited_account_two = userInfo.amount;
+            deposited_account_two = deposited_account_two.toNumber();
+            deposited_account_two.should.equal(100);
+        });
+
+        it('should distribute rewards proportionally according to staking amounts', async function() {
+            await this.libertas.transfer(account_two, 100, {from: account_one});                // Send 100 LIBERTAS to account2
+            await this.libertas.transfer(account_three, 200, {from: account_one});              // Send 200 LIBERTAS to account3
+            await this.libertas.approve(this.stakingPool.address, 100, {from: account_two});
+            await this.stakingPool.deposit(100, {from: account_two});                           // Deposit 100 LIBERTAS into staking pool from account2
+            await this.libertas.approve(this.stakingPool.address, 200, {from: account_three});
+            await this.stakingPool.deposit(200, {from: account_three});                         // Deposit 200 LIBERTAS from staking pool account3
+            
+            await this.libertas.approve(this.tipping.address, 6000, {from: account_one});
+            await this.tipping.transfer(account_four, 6000, {from: account_one});                // Transfer 600 LIBERTAS from account1 to account4
+
+            let balance_account_four = await this.libertas.balanceOf.call(account_four);
+            balance_account_four = balance_account_four.toNumber();
+            balance_account_four.should.equal(5400);                                            // Check account4 value is 5400
+
+            let balance_account_two = await this.libertas.balanceOf.call(account_two);
+            balance_account_two = balance_account_two.toNumber();
+            balance_account_two.should.equal(0);
+
+            await this.stakingPool.claim({from: account_two});
+            balance_account_two = await this.libertas.balanceOf.call(account_two);
+            balance_account_two = balance_account_two.toNumber();
+            balance_account_two.should.equal(90);                                               // Check account2 reward is 90
+
+            await this.stakingPool.withdraw(100, {from: account_two});
+            balance_account_two = await this.libertas.balanceOf.call(account_two);
+            balance_account_two = balance_account_two.toNumber();
+            balance_account_two.should.equal(190);                                              // Check account2 balance is 190 after withdraw
+
+            let balance_account_three = await this.libertas.balanceOf.call(account_three);
+            balance_account_three = balance_account_three.toNumber();
+            balance_account_three.should.equal(0);
+
+            await this.stakingPool.claim({from: account_three});
+            balance_account_three = await this.libertas.balanceOf.call(account_three);
+            balance_account_three = balance_account_three.toNumber();
+            balance_account_three.should.equal(180);                                             // Check account3 reward is 180
+
+            await this.stakingPool.withdraw(200, {from: account_three});
+            balance_account_three = await this.libertas.balanceOf.call(account_three);
+            balance_account_three = balance_account_three.toNumber();
+            balance_account_three.should.equal(380);                                            // Check account3 balance is 380 after withdraw
+        });
     });
 
     describe('Tipping Test', function() {
@@ -67,8 +143,6 @@ contract('Tipping', accounts => {
         });
 
         it('should transfer funds from sender to receiver with fee calculation', async function() {
-            const account_one = accounts[0];
-            const account_two = accounts[1];
             let org_balance_account_one = await this.libertas.balanceOf.call(account_one);
             let org_balance_account_two = await this.libertas.balanceOf.call(account_two);
             
