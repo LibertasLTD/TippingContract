15bcf4569f6da75233b18a002c66c8079b8ea696
IntegralTeam <repo@sfxdx.ru>
2021-10-18 21:05:49 +0300

    fixing bot and some bugs in contracts

--- contracts/interfaces/IERC20NameAndSymbol.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.7.0;
-
-interface IERC20NameAndSymbol  {
-    function name() external view returns(string memory);
-    function symbol() external view returns(string memory);
-}
--- scripts/bot_messanger/ethereum_server.js
+++ scripts/bot_messanger/ethereum_server.js
@@ -12,40 +12,10 @@ module.exports = (callback) => {
   ipc.config.id = 'ethereumBridge';
   ipc.config.retry = 1500;
 
+  let bridge;
+
   ipc.serve(
     () => {
-
-      console.log('Serving...');
-
-      // ipc.server.emit(
-      //   {
-      //     address : '127.0.0.1', //any hostname will work
-      //     port    : ipc.config.networkPort
-      //   },
-      //   'message',
-      //   {
-      //     from    : ipc.config.id,
-      //     message : 'Hello'
-      //   }
-      // );
-
-      // ipc.server.emit(
-      //   socket,
-      //   'message',
-      //   ''
-      // );
-
-      ipc.server.on(
-        'message',
-        (data, socket) => {
-          ipc.log('got a message : '.debug, data);
-          ipc.server.emit(
-            socket,
-            'message',
-            'Hello, Fantom bridge! Ethereum Bridge is serving you!'
-          );
-        }
-      );
 
       ipc.server.on(
         'socket.disconnected',
@@ -55,49 +25,49 @@ module.exports = (callback) => {
         }
       );
 
+      ipc.server.on(
+        "message",
+        (data, socket) => {
+          ipc.log("calling perform bridging to start: ", data);
+          const params = data.split('|');
+          bridge.performBridgingToStart(
+            params[0],
+            params[1],
+            new BN(params[2])
+          ).then((receipt) => {
+            ipc.log(`bridging to end performed with args: ${params} at ${receipt.transactionHash}`, data);
+          }).catch((error) => {
+            ipc.log(error);
+            callback();
+          });
+        }
+      );
       Bridge.at(process.env.BRIDGE_ON_ETHEREUM_ADDRESS).then((instance) => {
-
-        instance.events.RequestBridgingToEnd()
-          .on("connected", (subscriptionId) => {
-            ipc.log(`Connected to ${subscriptionId} subscription id.`);
-          })
+        bridge = instance;
+        // console.log(instance.RequestBridgingToEnd());
+        instance.RequestBridgingToEnd()
+          // .on("connected", (subscriptionId) => {
+          //   ipc.log(`Connected to ${subscriptionId} subscription id.`);
+          // })
           .on('data', (event) => {
             const eventData = event.returnValues;
-            ipc.of.server.emit(
+            ipc.log(`Gotta send event: ${event}`);
+            ipc.server.emit(
               {
                 address: '127.0.0.1',
                 port: ipc.config.networkPort
               },
-              "RequestBridgingToEnd",
+              "message",
               {
                 from: ipc.config.id,
                 message: `${eventData._tokenAtStart}|${eventData._from}|${eventData._to}|${eventData._amount}|LIBERTAS|LIBERTAS`
               }
             );
           })
-          .on('error', (error, receipt) => { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-            ipc.log(receipt);
-            callback();
-          });
-
-        ipc.of.server.on(
-          "RequestBridgingToStart",
-          (data) => {
-            ipc.log("calling perform bridging to start: ", data);
-            const params = data.split('|');
-            instance.performBridgingToStart(
-              params[0],
-              params[1],
-              new BN(params[2])
-            ).then((receipt) => {
-              ipc.log(`bridging to end performed with args: ${params} at ${receipt.transactionHash}`, data);
-            }).catch((error) => {
-              console.error(error);
-              callback();
-            });
-          }
-        );
-
+          // .on('error', (error, receipt) => { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+          //   ipc.log(error, '\n', receipt);
+          //   callback();
+          // });
       });
     }
   );
