f3d41bd54331efbf531e4697a1d752e8d68df481
IntegralTeam <repo@sfxdx.com>
2021-10-22 11:03:50 +0300

    coverage report added

--- truffle-config.js
+++ truffle-config.js
@@ -116,7 +116,8 @@ module.exports = {
     }
   },
   plugins: [
-    'truffle-plugin-verify'
+    'truffle-plugin-verify',
+    'solidity-coverage'
   ],
   api_keys: {
     etherscan: process.env.ETHERSCAN_API_KEY
