146dc679f23788cf6cf3fa169ad1e77541f6ea3a
IntegralTeam <repo@sfxdx.com>
2021-11-25 13:01:42 +0300

    fix scripts

--- scripts/test_bridge_from_start_to_end.js
+++ scripts/test_bridge_from_start_to_end.js
@@ -2,34 +2,34 @@ const { BN, ether, constants } = require('@openzeppelin/test-helpers');
 const { ZERO_ADDRESS } = constants;
 
 const Bridge = artifacts.require("Bridge");
-const LibertasUpgradeableProxy = artifacts.require("LibertasUpgradeableProxy");
+const LibertasToken = artifacts.require("LibertasToken");
 const IERC20 = artifacts.require("IERC20");
 
 module.exports = (callback) => {
-  Bridge.at(process.env.BRIDGE_ON_ETHEREUM_ADDRESS).then((instance) => {
-      console.log(`Got bridge instance at ${instance.address}`);
-      web3.eth.getAccounts().then((accounts) => {
-        console.log(`Got account: ${accounts[0]}`);
-        IERC20.at(LibertasUpgradeableProxy.address).then((token) => {
-          const amountToBridge = new BN('100');
-          console.log(`Got token: ${token.address}`);
-          token.approve(instance.address, amountToBridge, { from: accounts[0] }).then(() => {
-            token.balanceOf(accounts[0]).then((balance) => {
-              console.log(`Approved: ${amountToBridge}, with account balance: ${balance}`);
-              instance.requestBridgingToEnd(token.address, accounts[0], amountToBridge, { from: accounts[0] })
-                .then((receipt) => {
-                  console.log(receipt);
-                  token.balanceOf(accounts[0]).then((newBalance) => {
-                    console.log(`Performed request bridging to end with new balance of: ${newBalance}`);
-                    callback();
-                  });
-                }).catch((err) => {
-                  console.error(err);
-                  callback();
+    Bridge.at(process.env.BRIDGE_ON_ETHEREUM_ADDRESS).then((instance) => {
+        console.log(`Got bridge instance at ${instance.address}`);
+        web3.eth.getAccounts().then((accounts) => {
+            console.log(`Got account: ${accounts[0]}`);
+            IERC20.at(LibertasToken.address).then((token) => {
+                const amountToBridge = new BN('100');
+                console.log(`Got token: ${token.address}`);
+                token.approve(instance.address, amountToBridge, { from: accounts[0] }).then(() => {
+                    token.balanceOf(accounts[0]).then((balance) => {
+                        console.log(`Approved: ${amountToBridge}, with account balance: ${balance}`);
+                        instance.requestBridgingToEnd(token.address, accounts[0], amountToBridge, { from: accounts[0] })
+                            .then((receipt) => {
+                                console.log(receipt);
+                                token.balanceOf(accounts[0]).then((newBalance) => {
+                                    console.log(`Performed request bridging to end with new balance of: ${newBalance}`);
+                                    callback();
+                                });
+                            }).catch((err) => {
+                            console.error(err);
+                            callback();
+                        });
+                    });
                 });
             });
-          });
         });
-      });
-  });
+    });
 }
