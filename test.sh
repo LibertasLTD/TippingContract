a2bf53b44b1afc8de4d1dc3e6abbbbbe4caf60c1
IntegralTeam <repo@sfxdx.ru>
2023-02-27 18:52:26 +0300

    Clean trash.

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
- echo 'Running test scripts...'
- truffle exec ./scripts/test_bridge_from_start_to_end.js --network rinkeby
- truffle exec ./scripts/test_bridge_from_end_to_start.js --network fantom_ganache_fork
-
