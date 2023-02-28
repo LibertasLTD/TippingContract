55099d4a4dff497e15fdc1cc1d57c18ad37f71bb
IntegralTeam <repo@sfxdx.ru>
2023-02-28 13:23:46 +0300

    Add interface for tipping.

--- contracts/Tipping.sol
+++ contracts/Tipping.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IStakingPool.sol";
+import "./interfaces/ITipping.sol";
 
-contract Tipping is Ownable {
+contract Tipping is Ownable, ITipping {
     using SafeERC20 for IERC20;
 
     address public _STAKING_VAULT;
