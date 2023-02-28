4f3011b38dcf9095feef669aaf5ce8c331e3c1c6
IntegralTeam <repo@sfxdx.ru>
2023-02-28 11:53:15 +0300

    Fix staking pool interface.

--- contracts/interfaces/IStakingPool.sol
+++ contracts/interfaces/IStakingPool.sol
@@ -3,5 +3,21 @@
 pragma solidity ^0.8.18;
 
 interface IStakingPool {
-    function supplyReward(uint256) external;
+
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 amount);
+
+    function deposit(uint256 amount) external;
+
+    function withdraw(uint256 amount) external;
+
+    function claim() external;
+
+    function availableReward() external view returns (uint256);
+
+    function emergencyWithdraw() external;
+
+    function supplyReward(uint256 reward) external;
+
 }
