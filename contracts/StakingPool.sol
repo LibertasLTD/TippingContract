f10bf711dd971977f855d004d6096aa67723711b
IntegralTeam <repo@sfxdx.com>
2023-02-28 11:53:48 +0300

    Fix staking pool and interface. Rename libertas to odeum.

--- contracts/StakingPool.sol
+++ contracts/StakingPool.sol
@@ -3,15 +3,16 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "./interfaces/IStakingPool.sol";
 
-contract StakingPool is AccessControl {
-    using SafeERC20 for IERC20;
+contract StakingPool is AccessControl, IStakingPool {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    IERC20 public _LIBERTAS;
+    IERC20Upgradeable public ODEUM;
     mapping(address => UserInfo) public userInfo;
-    uint256 public accLibertasPerShare;
+    uint256 public accOdeumPerShare;
     uint256 public totalDeposits;
 
     uint256 public constant PRECISION = 1e12;
@@ -21,44 +22,45 @@ contract StakingPool is AccessControl {
         uint256 rewardDebt;
     }
 
+
     modifier onlyAdmin() {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "onlyAdmin");
         _;
     }
 
-    constructor(address LIBERTAS) {
-        _LIBERTAS = IERC20(LIBERTAS);
+    constructor(address ODEUM_) {
+        ODEUM = IERC20Upgradeable(ODEUM_);
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     function deposit(uint256 amount) external {
         UserInfo storage user = userInfo[msg.sender];
         if (user.amount >= 0 && amount > 0) {
-            uint256 pending = user.amount * accLibertasPerShare / PRECISION - user.rewardDebt;
+            uint256 pending = user.amount * accOdeumPerShare / PRECISION - user.rewardDebt;
             if (pending > 0) {
-                safeLIBERTASTransfer(msg.sender, pending);
+                safeOdeumTransfer(msg.sender, pending);
             }
-            _LIBERTAS.safeTransferFrom(msg.sender, address(this), amount);
+            ODEUM.safeTransferFrom(msg.sender, address(this), amount);
             user.amount = user.amount + amount;
             totalDeposits = totalDeposits + amount;
         }
-        user.rewardDebt = user.amount * accLibertasPerShare / PRECISION;
+        user.rewardDebt = user.amount * accOdeumPerShare / PRECISION;
         emit Deposit(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) external {
         UserInfo storage user = userInfo[msg.sender];
         require(user.amount >= amount, "withdrawTooMuch");
-        uint256 pending = user.amount * accLibertasPerShare / PRECISION - user.rewardDebt;
+        uint256 pending = user.amount * accOdeumPerShare / PRECISION - user.rewardDebt;
         if (pending > 0) {
-            safeLIBERTASTransfer(msg.sender, pending);
+            safeOdeumTransfer(msg.sender, pending);
         }
         if (amount > 0) {
             user.amount = user.amount - amount;
             totalDeposits = totalDeposits - amount;
-            _LIBERTAS.safeTransfer(msg.sender, amount);
+            ODEUM.safeTransfer(msg.sender, amount);
         }
-        user.rewardDebt = user.amount * accLibertasPerShare / PRECISION;
+        user.rewardDebt = user.amount * accOdeumPerShare / PRECISION;
         emit Withdraw(msg.sender, amount);
     }
 
@@ -66,7 +68,7 @@ contract StakingPool is AccessControl {
         UserInfo storage user
     ) internal view returns (uint256) {
         return
-            user.amount * accLibertasPerShare / PRECISION - user.rewardDebt;
+            user.amount * accOdeumPerShare / PRECISION - user.rewardDebt;
     }
 
     function claim() external {
@@ -74,9 +76,9 @@ contract StakingPool is AccessControl {
         UserInfo storage user = userInfo[msg.sender];
         uint256 pending = getPendingReward(user);
         if (pending > 0) {
-            safeLIBERTASTransfer(msg.sender, pending);
+            safeOdeumTransfer(msg.sender, pending);
         }
-        user.rewardDebt = user.amount * accLibertasPerShare / PRECISION;
+        user.rewardDebt = user.amount * accOdeumPerShare / PRECISION;
     }
 
     function availableReward() external view returns (uint256) {
@@ -91,7 +93,7 @@ contract StakingPool is AccessControl {
         uint256 amount = user.amount;
         user.amount = 0;
         user.rewardDebt = 0;
-        safeLIBERTASTransfer(msg.sender, amount);
+        safeOdeumTransfer(msg.sender, amount);
         emit EmergencyWithdraw(msg.sender, amount);
     }
 
@@ -99,19 +101,16 @@ contract StakingPool is AccessControl {
         if (totalDeposits == 0) {
             return;
         }
-        accLibertasPerShare = accLibertasPerShare + reward * PRECISION / totalDeposits;
+        accOdeumPerShare = accOdeumPerShare + reward * PRECISION / totalDeposits;
     }
 
-    function safeLIBERTASTransfer(address _to, uint256 _amount) internal {
-        uint256 totalBalance = _LIBERTAS.balanceOf(address(this));
+    function safeOdeumTransfer(address _to, uint256 _amount) internal {
+        uint256 totalBalance = ODEUM.balanceOf(address(this));
         if (totalBalance < _amount) {
-            _LIBERTAS.safeTransfer(_to, totalBalance);
+            ODEUM.safeTransfer(_to, totalBalance);
         } else {
-            _LIBERTAS.safeTransfer(_to, _amount);
+            ODEUM.safeTransfer(_to, _amount);
         }
     }
 
-    event Deposit(address indexed user, uint256 amount);
-    event Withdraw(address indexed user, uint256 amount);
-    event EmergencyWithdraw(address indexed user, uint256 amount);
 }
